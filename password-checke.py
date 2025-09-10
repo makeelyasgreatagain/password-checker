d0afd3cfc06f803ccf144da3333d7a1e7850f333
Elyas <elysignup@gmail.com>
2025-09-10 23:10:24 +0330

    Update password-checke.py
    Added GUI so you can visually check your password (thanks to me)

--- password-checke.py
+++ password-checke.py
@@ -1,30 +1,48 @@
 import re
+import tkinter as tk
+from tkinter import messagebox
 
-password = input("🔒 Please enter your password: ")
-score = 5
+def check_password():
+    password = entry_password.get()
+    score = 5
+    result = ""
 
-if len(password) < 8:
-    print("⚠️ Your password is too short. It should be at least 8 characters.")
-    score -= 1
+    if len(password) < 8:
+        result += "⚠️ Your password is too short. It should be at least 8 characters."
+        score -= 1
 
-if re.search("[A-Z]", password) == None:
-    print("⚠️ Add at least one upper case letter (A-Z) to strengthen your password.")
-    score -= 1
+    if re.search("[A-Z]", password) == None:
+        result += "⚠️ Add at least one upper case letter (A-Z) to strengthen your password."
+        score -= 1
 
-if re.search("[a-z]", password) == None:
-    print("⚠️ Include lower case letters (a-z) for better security.")
-    score -= 1
+    if re.search("[a-z]", password) == None:
+        result += "⚠️ Include lower case letters (a-z) for better security."
+        score -= 1
 
-if re.search("[0-9]", password) == None:
-    print("⚠️ Consider adding numbers (0-9) to make your password harder to guess.")
-    score -= 1
+    if re.search("[0-9]", password) == None:
+        result += "⚠️ Consider adding numbers (0-9) to make your password harder to guess."
+        score -= 1
 
-if re.search("[!@#$%^&*()_+=]", password) == None:
-    print("⚠️ Add special characters to boost password strength.")
-    score -= 1
+    if re.search("[!@#$%^&*()_+=]", password) == None:
+        result += "⚠️ Add special characters to boost password strength."
+        score -= 1
 
-print("\n🧮 Password strength score: ", score, "/ 5")
-if score < 5:
-    print("🚧 Your password could be stronger. Review the suggestions above and try again.")
-else:
-    print("✅ Excellent! Your password is strong. Just Remember to update it regulary.")
+    result += f"\n🧮 Password strength score: {score} / 5"
+    if score < 5:
+        result += "🚧 Your password could be stronger. Review the suggestions above and try again."
+    else:
+        result += "✅ Excellent! Your password is strong. Just Remember to update it regulary."
+
+    messagebox.showinfo("Result", result)
+
+root = tk.Tk()
+root.title = "Password Checker"
+
+tk.Label(root, text="Password: ").grid(row=0, column=0, padx=10, pady=10)
+entry_password = tk.Entry(root)
+entry_password.grid(row=0, column=1, padx=10, pady=5)
+
+btn_check = tk.Button(root, text="Check password", command=check_password)
+btn_check.grid(row=1,column=0, columnspan=2, pady=10)
+
+root.mainloop()
